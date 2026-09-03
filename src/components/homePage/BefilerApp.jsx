9cc20c9bb248bf9e1778e302bbec2569a460e5af
Maida Iftikhar <maidaiftikhar52@gmail.com>
2026-09-03 23:24:45 +0500

    Update code

--- src/components/homePage/BefilerApp.jsx
+++ src/components/homePage/BefilerApp.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FaFileAlt, FaShieldAlt, FaBell, FaHeadset } from "react-icons/fa";
 
 import mobileImage from "/src/assets/home/mobile-app.webp";
-import playStoreImage from "../../assets/home/logos/playstore.png";
 
 const Befilerapp = () => {
   const features = [
@@ -63,11 +62,16 @@ const Befilerapp = () => {
                   className="mt-4 inline-flex items-center rounded-lg transition duration-300 hover:scale-105 hover:shadow-lg"
                 >
                   <div className="flex h-11 items-center gap-2 rounded-md bg-black px-3 text-white shrink-0">
-                    <img
-                      src={playStoreImage}
-                      alt="Google Play"
-                      className="h-7 w-7 object-contain"
-                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-google-play"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M14.222 9.374c1.037-.61 1.037-2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055zM1 13.396V2.603L6.846 8zM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27" />
+                    </svg>
 
                     <div className="leading-none text-left">
                       <p className="text-[8px] uppercase">Get it on</p>
